67361136edcf9f93b7f5ad41f14276e208db9dde
Joseph MacDonald <josephmichaelmacdonald@gmail.com>
2020-11-12 15:33:21 -0600

    probably quitting here

--- animate.js
+++ animate.js
@@ -1,5 +1,22 @@
-//function DoAnimation() {
-//  var targetElement = document.getElementById("target");
-//  targetElement.className = "animate";
+console.log('js');
 
-  console.log('js');
+function DoAnimationR() {
+  var targetElement = document.getElementById("munsch");
+  targetElement.className = "animate-r";
+}
+
+function DoAnimationL() {
+var targetElement = document.getElementById("chops");
+targetElement.className = "animate-l";
+}
+
+// function animateLoser() {
+//   targetElement.className = "loser";
+// }
+//
+// function animateJason() {
+//   var targetElement = document.getElementById("jason");
+//   DoAnimationR();
+//   animateLoser();
+// 
+// }
